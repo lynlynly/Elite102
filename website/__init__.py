66a952d3ffe419771f265fc62ed15afe9a2ae584
lynlynly <130028994+lynlynly@users.noreply.github.com>
2023-04-30 23:09:22 -0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import CustomerInfo, Note
+    from .models import CustomerInfo
     
     with app.app_context():
         db.create_all()
