452642cdc75bace6a9d3a94edccb20a3ce3bce7a
lynlynly <130028994+lynlynly@users.noreply.github.com>
2023-04-30 23:09:48 -0500

    Update models.py

--- website/models.py
+++ website/models.py
@@ -3,13 +3,6 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
 class AccountInfo(db.Model): 
     account_num = db.Column(db.Integer, primary_key=True)
     account_type = db.Column(db.String(150))
